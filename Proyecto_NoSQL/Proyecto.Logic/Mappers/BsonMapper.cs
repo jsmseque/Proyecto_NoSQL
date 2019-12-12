8d42823fab73c13fc3c15625155d27324ee2bfd9
JOHAN SEQUEIRA <jsequeira90598@ufide.ac.cr>
2019-12-11 19:00:56 -0600

    Mapper y Model

---
+++ Proyecto_NoSQL/Proyecto.Logic/Mappers/BsonMapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+using Proyecto.Model.MisModelos;
+
+
+namespace Proyecto.Logic.Mappers
+{
+    class BsonMapper
+    {
+        public void Map()
+        {
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Cliente)))
+            {
+                BsonClassMap.RegisterClassMap<Cliente>(
+                    cliente =>
+                {
+                    cliente.MapIdProperty(p => p.ClienteId).SetIdGenerator(new StringObjectIdGenerator());
+                    cliente.MapProperty(p => p.Cedula).SetElementName("cedula");
+                    cliente.MapProperty(p => p.Nombre).SetElementName("nombre");
+                    cliente.MapProperty(p => p.PrimerApellido).SetElementName("primerApellido");
+                    cliente.MapProperty(p => p.SegundoApellido).SetElementName("segundoApellido");
+                    cliente.MapProperty(p => p.Telefonos).SetElementName("telefonos");
+                    cliente.MapProperty(p => p.Email).SetElementName("email");
+                    cliente.MapProperty(p => p.Direccion).SetElementName("direccion");          
+                    cliente.SetIgnoreExtraElements(true);
+                });
+            }
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Telefono)))
+            {
+                BsonClassMap.RegisterClassMap<Telefono>
+                    (telefono =>
+                    {
+                        telefono.MapProperty(p => p.Tipo).SetElementName("tipo");
+                        telefono.MapProperty(p => p.Numero).SetElementName("numero");
+                    });
+            }
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Direccion)))
+            {
+                BsonClassMap.RegisterClassMap<Direccion>
+                    (direccion =>
+                    {
+                        direccion.MapProperty(p => p.Pais).SetElementName("pais");
+                        direccion.MapProperty(p => p.Provincia).SetElementName("provincia");
+                        direccion.MapProperty(p => p.Canton).SetElementName("canton");
+                        direccion.MapProperty(p => p.Distrito).SetElementName("distrito");
+                    });
+            }
+        } 
+    }
+}
---
+++ Proyecto_NoSQL/Proyecto.Model/MisModelos/Cliente.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.IdGenerators;
+
+namespace Proyecto.Model.MisModelos
+{
+    //[MetadataType(typeof(ClienteMetadata))]
+    //[BsonIgnoreExtraElements]
+   public class Cliente
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        public string ClienteId { get; set; }
+
+        [BsonElement("cedula")]
+        public int Cedula { get; set; }
+
+        [BsonElement("nombre")]
+        public string Nombre { get; set; }
+
+        [BsonElement("primerApellido")]
+        public string PrimerApellido { get; set; }
+
+        [BsonElement("segundoApellido")]
+        public string SegundoApellido { get; set; }
+
+        [BsonElement("telefonos")]
+        public Telefono[] Telefonos { get; set; }
+
+        [BsonElement("email")]
+        public Telefono[] Email { get; set; }
+
+        [BsonElement("direccion")]
+        public Direccion Direccion { get; set; }
+
+        //[BsonExtraElements]
+        // public BsonDocument Metadata { get; set; }
+    }
+
+    public class Telefono
+    {
+        public string Tipo { get; set; }
+        public string Numero { get; set; }
+    }
+
+    public class Direccion
+    {
+        public string Pais { get; set; }
+        public string Provincia { get; set; }
+        public string Canton { get; set; }
+        public string Distrito { get; set; }
+
+    }
+
+}
